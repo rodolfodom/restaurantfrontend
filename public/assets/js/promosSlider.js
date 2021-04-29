7a5123d28e08b6701a08ec13110d45e44dcba8a6
rodolfodom <rodolfodominguez60@aragon.unam.mx>
2021-04-29 08:56:51 -0500

    fix/slider: I restructured the slider organization to js modules

---
+++ public/assets/js/promosSlider.js
@@ -0,0 +1,22 @@
+export default function promosSlider() {
+    const swiper = new Swiper('.swiper-container', {
+                slidesPerView: 1,
+                spaceBetween: 30,
+                loop: true,
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                    dynamicBullets: true
+                },
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                },
+            });
+}
+
+
+
+
+
+
---
+++ public/main.js
@@ -0,0 +1,7 @@
+import promosSlider from "./assets/js/promosSlider.js";
+
+const d = document;
+
+d.addEventListener("DOMContentLoaded", ()=>{
+    promosSlider();
+})
